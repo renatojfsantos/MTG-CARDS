1930ac36758049bebefa22ab29a57c1b5840797b
Renato Santos <renato_jfs@hotmail.com>
2022-01-29 01:16:26 -0300

    Adjust imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Gupter_400Regular,
+  Gupter_500Medium,
+  Gupter_700Bold,
+} from '@expo-google-fonts/gupter';
+
+import theme from './theme/index';
+import { Home } from './screens/Home';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+  const [fontsLoaded] = useFonts({
+    Gupter_400Regular,
+    Gupter_500Medium,
+    Gupter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return(
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
